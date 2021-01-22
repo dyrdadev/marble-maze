cd34d8975b8f2aaebb705e63801ac62e4fd1d72c
Daniel <dyrda@in.tum.de>
2021-01-22 09:39:04 +0100

    Refactoring

--- Assets/Features/GameBoard/GameBoard.cs
+++ Assets/Features/GameBoard/GameBoard.cs
@@ -1,26 +1,15 @@
 using UnityEngine;
 using System.Collections;
 
-
-// Script that handles the rotation input into the marble game
-
 public class GameBoard : MonoBehaviour {
-	
-	// maximum rotation angle for accelerometer input
 	public float maxAngle = 60.0f;
-	
-	// current x and y euler angle
 	float xAngle, yAngle;
-
-
-    AccelerometerInput accelerometerInput;
+	AccelerometerInput accelerometerInput;
 
     // Use this for initialization
     void Start () {
-
-        accelerometerInput = GetComponent<AccelerometerInput>();
-        
-		xAngle = 0;
+	    accelerometerInput = GetComponent<AccelerometerInput>();
+	    xAngle = 0;
 		yAngle = 0;
 	}
 	
