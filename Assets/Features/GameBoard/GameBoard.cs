5a9f3ae9fcbaa0dfdf197e1e243596a24f0b7391
Daniel <dyrda@in.tum.de>
2021-01-22 09:40:09 +0100

    Refactoring

--- Assets/Features/GameBoard/GameBoard.cs
+++ Assets/Features/GameBoard/GameBoard.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public class GameBoard : MonoBehaviour {
 	public float maxAngle = 60.0f;
 	float xAngle, yAngle;
-	AccelerometerInput accelerometerInput;
-
-    // Use this for initialization
-    void Start () {
+	AccelerometerInput accelerometerInput; 
+	
+	// Use this for initialization
+	void Start () {
 	    accelerometerInput = GetComponent<AccelerometerInput>(); 
 	    xAngle = 0; 
 	    yAngle = 0;
